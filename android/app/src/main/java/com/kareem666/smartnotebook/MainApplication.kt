160e1d73934d3edacd22564b61872f8a9d0376a7
karim-99-99 <kareem.hossam99@yahoo.com>
2026-03-28 06:01:53 +0200

    EAS Android: more Gradle heap + md

--- android/app/src/main/java/com/kareem666/smartnotebook/MainApplication.kt
+++ android/app/src/main/java/com/kareem666/smartnotebook/MainApplication.kt
@@ -1,6 +1,7 @@
 package com.smartnotebook
 
 import android.app.Application
+import android.content.res.Configuration
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
@@ -11,47 +12,43 @@ import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
 import expo.modules.ApplicationLifecycleDispatcher
-import expo.modules.ExpoReactHostFactory
-import android.content.res.Configuration
+import expo.modules.ReactNativeHostWrapper
 
 class MainApplication : Application(), ReactApplication {
-  private val appPackages: List<ReactPackage>
-    get() = PackageList(this).packages
 
   override val reactNativeHost: ReactNativeHost =
-      object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> =
-            appPackages.toMutableList().apply {
-              // Packages that cannot be autolinked yet can be added manually here
+      ReactNativeHostWrapper(
+          this,
+          object : DefaultReactNativeHost(this) {
+            override fun getPackages(): List<ReactPackage> =
+                PackageList(this).packages.apply {
+                  // Packages that cannot be autolinked yet can be added manually here
+                }
+
+            override fun getJSMainModuleName(): String = "index"
+
+            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
+            override fun getJSBundleFile(): String? {
+              if (BuildConfig.DEBUG) {
+                return null
+              }
+              return super.getJSBundleFile()
             }
 
-        override fun getJSMainModuleName(): String = "index"
-
-        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-
-        override fun getJSBundleFile(): String? {
-          // Use WiFi IP for Metro bundler when in debug mode
-          if (BuildConfig.DEBUG) {
-            // This allows the app to connect to Metro via WiFi IP
-            return null // Use default, but we'll configure via DevSettings
-          }
-          return super.getJSBundleFile()
-        }
-
-        override val isHermesEnabled: Boolean = true
-      }
+            override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+            override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+          })
 
   override val reactHost: ReactHost
-    get() = ExpoReactHostFactory.getDefaultReactHost(
-        applicationContext,
-        appPackages,
-        "index",
-    )
+    get() = ReactNativeHostWrapper.createReactHost(applicationContext, reactNativeHost)
 
   override fun onCreate() {
     super.onCreate()
     SoLoader.init(this, OpenSourceMergedSoMapping)
-    load()
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      load()
+    }
     ApplicationLifecycleDispatcher.onApplicationCreate(this)
   }
 
@@ -60,4 +57,3 @@ class MainApplication : Application(), ReactApplication {
     ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
   }
 }
-
